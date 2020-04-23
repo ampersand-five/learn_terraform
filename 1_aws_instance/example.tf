f9fd7f7cb19a6420d335002ea79c496dc005c950
Mike Forsyth <9754171+canada11@users.noreply.github.com>
2020-04-22 21:20:28 -0600

    Elastic ip to first page example

--- 1_aws_instance/example.tf
+++ 1_aws_instance/example.tf
@@ -1,19 +1,31 @@
 provider "aws" {
-  profile = "de-dev"
+  profile = ""
   region  = "us-west-2"
 }
 
-resource "aws_instance" "delete_me_after_" {
+resource "aws_instance" "_delete_me_after_" {
   ami             = "ami-0d6621c01e8c2de2c"
   instance_type   = "t2.micro"
   subnet_id       = "subnet-"
 #   security_groups = ["sg-"]
+
+    provisioner "local-exec" {
+        command = "echo ${aws_instance._delete_me_after_.public_ip} > ip_address.txt"
+    }
+
   vpc_security_group_ids = ["sg-"]
   tags = {
-    Name = "delete_me_after_"
+    Name = "_delete_me_after_"
     bc = ""
     created_by = ""
     org = ""
     team = ""
   }
+}
+
+
+# adding an elastic ip
+resource "aws_eip" "ip" {
+    vpc = true
+    instance = aws_instance._delete_me_after_.id
 }
